7f165883653c0434352138c293bac8cf4eae65d2
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2024-01-12 11:54:58 -0300

    Creation for the EmployeeModel.cs

---
+++ src/GraphQl_API/GraphQl_API/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿namespace GraphQl_API.Models;
+
+public class EmployeeModel
+{
+    /// <summary>
+    /// Employee
+    /// </summary>
+    /// <param name="Id"></param>
+    /// <param name="Name"></param>
+    /// <param name="Age"></param>
+    /// <param name="DeptId"></param>
+    public record Employee(int Id, string Name, int Age, int DeptId);
+    
+    
+    
+}
