380e4cc4534745fc05d9ed5c927bb63a2266cdc0
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2024-01-12 15:07:44 -0300

    EmployeeDetailsType Field mapping class created

--- src/GraphQl_API/GraphQl_API/Models/EmployeeModel.cs
+++ src/GraphQl_API/GraphQl_API/Models/EmployeeModel.cs
@@ -1,4 +1,6 @@
-﻿namespace GraphQl_API.Models;
+﻿using GraphQL.Types;
+
+namespace GraphQl_API.Models;
 
 public class EmployeeModel
 {
@@ -17,4 +19,33 @@ public class EmployeeModel
     /// <param name="Id"></param>
     /// <param name="Name"></param>
     public record Department(int Id, string Name);
+
+   /// <summary>
+   /// EmployeeDetails
+   /// </summary>
+    public class EmployeeDetails
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? DeptName { get; set; }
+    }
+
+   /// <summary>
+   /// Employee details type
+   /// </summary>
+    public class EmployeeDetailsType : ObjectGraphType<EmployeeDetails>
+    {
+        // EmployeeDetails is the model for API response but graphQL can’t understand this hence we need to create a mapping and as a result,
+        // we create EmployeeDetailsType Field mapping class.
+        public EmployeeDetailsType()
+        {
+            Field(x => x.Id);
+            Field(x => x.Name);
+            Field(x => x.Age);
+            Field(x => x.DeptName);
+            
+        }
+    }
+    
 }
