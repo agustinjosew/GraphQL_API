a6bcb3244a65f5ef11bd733a0a383defbd86278a
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2024-01-12 21:14:24 -0300

    Creation of the GraphQL Query

---
+++ src/GraphQl_API/GraphQl_API/Queries/EmployeeQuery.cs
@@ -0,0 +1,32 @@
+﻿
+
+using GraphQL;
+using GraphQl_API.Services.Interfaces;
+using GraphQL.Types;
+using static GraphQl_API.Models.EmployeeModel;
+
+namespace GraphQl_API.Queries;
+
+public class EmployeeQuery :ObjectGraphType
+{
+    public EmployeeQuery(IEmployeeService employeeService){
+        // mapping against EmployeeService methods to fetch data
+        Field<ListGraphType<EmployeeDetailsType>>(Name = "Employees", 
+            resolve : x => employeeService.GetEmployees());
+        
+        Field<ListGraphType<EmployeeDetailsType>>(Name = "Employee", 
+            arguments: new QueryArguments(new QueryArgument<IntGraphType> {Name = "id"}),
+            resolve: x => employeeService.GetEmployee(x.GetArgument<int>("id")));
+    }
+}
+
+/// <summary>
+///  Map the Employee query (EmployeeQuery) class to the GraphQL
+///  schema by creating a class (EmployeeDetailsSchema) that inherits Schema.
+/// </summary>
+public class EmployeeDetailsSchema : Schema
+{
+    public EmployeeDetailsSchema(IServiceProvider serviceProvider) : base(serviceProvider) {
+        Query = serviceProvider.GetRequiredService<EmployeeQuery>();
+    }
+}
